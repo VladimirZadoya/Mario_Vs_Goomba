ce35d8c0bd3d28f6b6bc467d725712f086e977b0
VladimirZadoya <kegeralpha@gmail.com>
2024-06-12 01:35:06 +0300

    eng._vers._4

--- main.py
+++ main.py
@@ -4,27 +4,27 @@ import random
 pygame.init()
 joke = pygame.mixer.Sound(path.join( 'game-over-super-mario-made-with-Voicemod (mp3cut.net).mp3'))
 joke.set_volume(5)
-# Выставляем широту и длину экрана в переменных
+# Setting the width and length of the screen in variables
 W = 800
 H = 600
-# добавление музыки
+# adding music
 pygame.mixer.music.load("super-mario-saundtrek.mp3")
 pygame.mixer.music.play(-1)
-# создаем сам экран
+# create the screen itself
 screen = pygame.display.set_mode((W, H))
-# создаем счетчик кадров в секунду
+# create a frames per second counter
 FPS = 60
-# объект часов
+# clock object
 clock = pygame.time.Clock()
 
-# создание всех необходимых переменных (шрифт, game_over которая будет обозначать проигрыш или нет)
+# creating all the necessary variables (font, game_over which will indicate loss or not)
 font_path = 'SuperMario256.ttf'
 font_xsmall = pygame.font.SysFont('arial_bold', 36)
 font_large = pygame.font.Font(font_path, 48)
 font_small = pygame.font.Font(font_path, 24)
 
 game_over = False
-# поверхности которые будут появляться при проигрыше и расположение их на экране
+# surfaces that will appear when losing and their location on the screen
 retry_text = font_small.render('PRESS ANY KEY', True, (255, 255, 255))
 retry_rect = retry_text.get_rect()
 retry_rect.midtop = (W // 2, H // 2)
@@ -32,14 +32,14 @@ retry_rect.midtop = (W // 2, H // 2)
 retry_text1 = font_small.render('GAME OVER', True, (255, 255, 255))
 retry_rect1 = retry_text1.get_rect()
 retry_rect1.midtop = (400, 220)
-# добавление файла кнопки информации об игровых механиках
+# adding a button file about game mechanics information
 info_image = pygame.image.load('cnopka3llp.png')
 info_image = pygame.transform.scale(info_image, (160, 40))
 retry_rect2 = info_image.get_rect()
 retry_rect2.midtop = (W // 2, H - 50)
 
 show_info_window = False
-# текст для плашки информация
+# text for information dies
 info_text = font_small.render('Information', True, (255, 255, 255))
 info_rect = info_text.get_rect()
 info_rect.midtop = (W // 2, 200)
@@ -63,79 +63,79 @@ info_rect6 = info_text6.get_rect()
 info_rect6.midtop = (W // 2 - 90, 380)
 info_window_rect = pygame.Rect(W // 2 - 200, H // 2 - 115, 400, 230)
 
-# добавления деревьев а также редактирование их в нужные размеры
+# adding trees and editing them to the desired size
 tree_image = pygame.image.load('images_tree.png')
 tree_image = pygame.transform.scale(tree_image, (100, 140))
 tree_image1 = pygame.image.load('images_tree.png')
 tree_image1 = pygame.transform.scale(tree_image1, (100, 140))
 
-# добавления облаков а также редактирование их в нужные размеры
+# adding clouds and editing them to the desired size
 cloud_image = pygame.image.load('pixel-art-cloud-vector-icon-for-8bit-game-on-white-background_360488-650.jpg.png')
 cloud_image = pygame.transform.scale(cloud_image, (100, 100))
 cloud_image1 = pygame.image.load('pixel-art-cloud-vector-icon-for-8bit-game-on-white-background_360488-650.jpg.png')
 cloud_image1 = pygame.transform.scale(cloud_image1, (100, 100))
 
-# добавление файла земли а также редактирование его в нужные размеры
+# adding a land file and editing it to the required dimensions
 ground_image = pygame.image.load('Снимок экрана 2024-05-04 в 17.27.10.png')
 ground_image = pygame.transform.scale(ground_image, (804, 60))
 GROUND_H = ground_image.get_height()
 
-# добавление файла uev,s а также редактирование его в нужные размеры
+# adding the uev,s file and editing it to the desired size
 enemy = pygame.image.load('gimi.png')
 enemy = pygame.transform.scale(enemy, (80, 80))
 
-# добавление файла мертвого гумбы а также редактирование его в нужные размеры
+# adding a dead goomba file and editing it to the desired size
 enemy_image = pygame.image.load('goomba_mini.png')
 enemy_image = pygame.transform.scale(enemy_image, (80, 80))
 
-# добавление файла mario а также редактирование его в нужные размеры
+# adding a mario file and editing it to the desired size
 player_image_right = pygame.image.load('images1.png')
 player_image_right = pygame.transform.scale(player_image_right, (80, 60))
 player_image_left = pygame.transform.flip(player_image_right, True, False)
 
 # ////////////////////////////////////////////////////////////////////////////////////////////////
-# класс для объявления сущностей (марио, гумба)
+# class for declaring entities (Mario, Goomba)
 class Entity:
-    # создание главной функции конструктора и создания в нем переменных для рисунка марио или гумба, скорости
+    # creating the main constructor function and creating variables in it for the Mario or Goomba drawing, speed
     def __init__(self, image):
         self.image = image
         self.rect = self.image.get_rect()
-        # скорость по x и y
+        # speed in x and y
         self.x_speed = 0
         self.y_speed = 0
 
-        self.speed = 5  # основная скорость
-        # переменные для проверки упали ли мы в пропасть или живы ли мы или нет
+        self.speed = 5  # main speed
+        # variables to check whether we fell into the abyss or whether we are alive or not
         self.is_out = False
         self.is_dead = False
-        # скорость прыжка
+        # jump speed
         self.jump_speed = -12
-        # скорость гравитации
+        # speed of gravity
         self.gravity = 0.4
-        # переменная для того чтобы мы знали находимся ли мы на земле или нет
+        # variable so that we know whether we are on the ground or not
         self.is_grounded = False
 
-    # функция для управления пользовательским вводом. Она будет пустая, но я буду предопределять для того кому она нужна, в этом случае для марио
+    # function to manage user input. It will be empty, but I will predetermine it for the one who needs it, in this case for Mario
     def handle_input(self):
         pass
 
-    # функция для убивания нашей сущности
+    # function to kill our entity
     def kill(self, dead_image):
-        # замена картинки сущности на сплющенную
+        # replacing the entity image with a flattened one
         self.image = dead_image
-        # будем говорить что наша сущность мертва
+        # let's say that our essence is dead
         self.is_dead = True
-        # эффект отлитания при смерти в сторону и подпрыгивания
+        # the effect of casting to the side when dying and bouncing
         self.x_speed = -self.x_speed
         self.y_speed = self.jump_speed
 
-    # функция перемещения
+    # move function
     def update(self):
         self.rect.x += self.x_speed
         self.y_speed += self.gravity
         self.rect.y += self.y_speed
 
-        # проверка на то, что умерли мы или нет
+        # checking whether we died or not
         if self.is_dead:
             if self.rect.top > GROUND_H:
                 self.is_out = True
@@ -148,61 +148,60 @@ class Entity:
                 self.y_speed = 0
                 self.rect.bottom = H - GROUND_H
 
-    # функция для отрисовки сущности
+    # function to render an entity
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
 
 # ////////////////////////////////////////////////////////////////////////////////////////////////
-# класс для марио, унаследованный из класса entity
+# class for Mario, inherited from entity class
 class Player(Entity):
     def __init__(self):
         super().__init__(player_image_right)  # передаем загруженную картинку игрока
         self.respawn()
 
-    # функция для влияния скорости игрока
+    # function to influence player speed
     def handle_input(self):
         self.x_speed = 0
 
-        # реализация клавиши для движения влево
+        # implementation of a key to move left
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.x_speed = -self.speed  # двигаем влево
-            self.image = player_image_left  # изменяем картинку на левую
+            self.x_speed = -self.speed  # move left
+            self.image = player_image_left  # change the image to the left
 
-        # реализация клавиши для движения вправо
+        # implementation of a key to move right
         elif keys[pygame.K_d]:
-            self.x_speed = self.speed  # двигаем вправо
-            self.image = player_image_right  # изменяем картинку на правую
+            self.x_speed = self.speed  # move to the right
+            self.image = player_image_right  # change the image to the right one
 
-        # реализация клавиши для прыжка
+        # implementation of the jump key
         if self.is_grounded and keys[pygame.K_SPACE]:
             self.is_grounded = False
             self.jump()
 
-    # функция для рестарта игры
+    # function to restart the game
     def respawn(self):
         self.is_out = False
         self.is_dead = False
         show_info_window = False
         self.rect.midbottom = (W // 2, H - GROUND_H)
 
-    # функция прыжка
+    # jump function
     def jump(self):
         self.y_speed = self.jump_speed
 
 
 # /////////////////////////////////////////////////////////
-# класс гумба или класса врага
+# goomba class or enemy class
 class Goomba(Entity):
     def __init__(self):
         super().__init__(enemy)
         self.spawn()
 
-    # функция для того чтобы гумба появлялся
+    # function for making a goomba appear
     def spawn(self):
-        direction = random.randint(0, 1)  # выпадение гумба справа (0) или слева (1)
-
+        direction = random.randint(0, 1)  # Goomba falling out on the right (0) or left (1)
         if direction == 0:
             self.x_speed = self.speed
             self.rect.bottomright = (0, 0)
@@ -210,35 +209,35 @@ class Goomba(Entity):
             self.x_speed = -self.speed
             self.rect.bottomleft = (W, 0)
 
-    # добавление уже существующей функции и доработка её
+    # adding an existing function and improving it
     def update(self):
         super().update()
 
-        # проверка на то что ушли ли гумбы в правую или левую часть экрана
+        # checking whether goombas have gone to the right or left side of the screen
         if self.x_speed > 0 and self.rect.left > W or self.x_speed < 0 and self.rect.right < 0:
             self.is_out = True
 
 
-player = Player()  # объект класса
+player = Player()  # class object
 
-# переменная для подсчитывания наших очков в игре
+# variable for counting our points in the game
 score = 0
 
-# список который будет хранить всех гумб на экране
+# a list that will store all goombas on the screen
 goombas = []
-# переменная которая будет хранить в себе начальную задержку гумбы
+# a variable that will store the initial delay of the goomba
 INIT_DELAY = 2000
-# переменная которая будет обозначать как часто спавнятся гумбы
+# a variable that will indicate how often goombas spawn
 spawn_delay = INIT_DELAY
-# переменная которая нужна для того чтобы усложнять игру или сокращать время спавна гумб
+# a variable that is needed to complicate the game or reduce the spawn time of goombas
 DECREASE_BASE = 1.01
-# переменная для того чтобы знать время последнего спавна
+# variable to know the last spawn time
 the_last_spawn_time = pygame.time.get_ticks()
 
-# игровой цикл
+# game loop
 running = True
 while running:
-    # перебираю события закрытия окна
+    # going through the window closing events
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             running = False
@@ -256,25 +255,25 @@ while running:
                 the_last_spawn_time = pygame.time.get_ticks()
                 player.respawn()
                 goombas.clear()
-    # ограничиваю игру по кадрам в секунду
+    # I'm limiting the game to frames per second
     clock.tick(FPS)
 
-    screen.fill((92, 148, 252))  # залил экран
-    # отрисовка облаков
+    screen.fill((92, 148, 252))  # flooded the screen
+    # cloud rendering
     screen.blit(cloud_image1, (600, 100))
 
     screen.blit(cloud_image, (100, 100))
-    # отрисовка деревьев
+    # tree drawing
     screen.blit(tree_image, (0, 410))
     screen.blit(tree_image1, (700, 410))
-    # отрисовка земли
+    # land rendering
     screen.blit(ground_image, (0, H - GROUND_H))
 
-    # поверхность для отрисовки очков и переменная
+    # surface for drawing glasses and variable
     score_text = font_large.render(str(score), True, (255, 255, 255))
     score_rect = score_text.get_rect()
 
-    # проверка на то что вылетел ли марио за границы
+    # checking to see if mario's out of bounds
     if player.is_out:
         score_rect.midbottom = (W // 2, H // 2)
         screen.blit(retry_text, retry_rect)
@@ -282,21 +281,21 @@ while running:
         screen.blit(info_image, retry_rect2)
     else:
 
-        # добавление сущности
+        # entity addition
         player.update()
         player.draw(screen)
-        # текущее игровое время
+        # current playing time
         now_time = pygame.time.get_ticks()
-        # время которое прошло с момента предыдущего спавна
+        # time elapsed since the previous spavin
         elapsed = now_time - the_last_spawn_time
-        # добавление сущности
+        # entity addition
 
-        # проверка на то что прошло ли время с момента предыдущего спавна
+        # check to see if time has elapsed since the previous spawning
         if elapsed > spawn_delay:
             the_last_spawn_time = now_time
             goombas.append(Goomba())
 
-        # редактирование гумб (удалять тех кто ушел за поле)
+        # editing the goombas (remove those who have left the field)
         for goomba in list(goombas):
             if goomba.is_out:
                 goombas.remove(goomba)
@@ -325,9 +324,9 @@ while running:
         screen.blit(info_text5, info_rect5)
         screen.blit(info_text6, info_rect6)
 
-    # расположение счетчика
+    # meter location
     score_rect.midtop = (W // 2, H // 5)
-    # выставили счет очков в игре
+    # put up a score in the game
     screen.blit(score_text, score_rect)
-    pygame.display.flip()  # обновил экран для того чтоб все отображалось
+    pygame.display.flip()  # I refreshed the screen so that everything would show up.
 quit()
